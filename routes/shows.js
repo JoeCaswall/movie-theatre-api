bdd892dcf317ae5a6caf37be28b239df83b0a4ea
JoeCaswall <joe.caswall@gmail.com>
2023-11-10 16:05:46 +0000

    try/catch added

--- routes/shows.js
+++ routes/shows.js
@@ -7,42 +7,62 @@ const showRouter = express.Router();
 showRouter.use(express.json());
 showRouter.use(express.urlencoded());
 
-showRouter.get("/", async (req, res) => {
-  const shows = await Show.findAll();
-  res.json(shows);
+showRouter.get("/", async (req, res, next) => {
+  try {
+    const shows = await Show.findAll();
+    res.json(shows);
+  } catch (error) {
+    next(error);
+  }
 });
 
-showRouter.get("/:id", async (req, res) => {
-  const show = await Show.findByPk(req.params.id);
-  res.json(show);
+showRouter.get("/:id", async (req, res, next) => {
+  try {
+    const show = await Show.findByPk(req.params.id);
+    res.json(show);
+  } catch (error) {
+    next(error);
+  }
 });
 
-showRouter.get("/:id/users", async (req, res) => {
-  const showUsers = await Show.findOne({
-    where: { id: req.params.id },
-    include: User,
-  });
-  res.json(showUsers.users);
+showRouter.get("/:id/users", async (req, res, next) => {
+  try {
+    const showUsers = await Show.findOne({
+      where: { id: req.params.id },
+      include: User,
+    });
+    res.json(showUsers.users);
+  } catch (error) {
+    next(error);
+  }
 });
 
-showRouter.get("/genres/:genre", async (req, res) => {
-  const shows = await Show.findAll({
-    where: { genre: req.params.genre },
-  });
-  res.json(shows);
+showRouter.get("/genres/:genre", async (req, res, next) => {
+  try {
+    const shows = await Show.findAll({
+      where: { genre: req.params.genre },
+    });
+    res.json(shows);
+  } catch (error) {
+    next(error);
+  }
 });
 
 showRouter.post(
   "/",
   [check("title").isLength({ min: 2, max: 25 }).trim()],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.json({ error: errors.array() });
-    } else {
-      const newShow = await Show.create(req.body);
-      newShow.genre = newShow.genre.toLowerCase().split(" ").join("-");
-      res.json(newShow);
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const newShow = await Show.create(req.body);
+        newShow.genre = newShow.genre.toLowerCase().split(" ").join("-");
+        res.json(newShow);
+      }
+    } catch (error) {
+      next(error);
     }
   }
 );
@@ -50,19 +70,23 @@ showRouter.post(
 showRouter.patch(
   "/:id/submitrating",
   [check("rating").not().isEmpty().trim()],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.json({ error: errors.array() });
-    } else {
-      const show = await Show.findByPk(req.params.id);
-      let currentRating = show.rating;
-      //if it isn't rated yet, make starting rating = first users rating
-      if (currentRating === null) {
-        currentRating = req.body.rating;
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const show = await Show.findByPk(req.params.id);
+        let currentRating = show.rating;
+        //if it isn't rated yet, make starting rating = first users rating
+        if (currentRating === null) {
+          currentRating = req.body.rating;
+        }
+        await show.update({ rating: (currentRating + req.body.rating) / 2 });
+        res.json(show);
       }
-      await show.update({ rating: (currentRating + req.body.rating) / 2 });
-      res.json(show);
+    } catch (error) {
+      next(error);
     }
   }
 );
@@ -71,21 +95,34 @@ showRouter.patch(
   "/:id/available",
   [check("available").not().isBoolean()], //availability should be a boolean
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.json({ error: errors.array() });
-    } else {
-      const show = await Show.findByPk(req.params.id);
-      await show.update({ available: req.body.available });
-      res.json(show);
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ error: errors.array() });
+      } else {
+        const show = await Show.findByPk(req.params.id);
+        await show.update({ available: req.body.available });
+        res.json(show);
+      }
+    } catch (error) {
+      next(error);
     }
   }
 );
 
 showRouter.delete("/:id", async (req, res) => {
-  await Show.destroy({ where: { id: req.params.id } });
-  const allShows = await Show.findAll();
-  res.json(allShows);
+  try {
+    await Show.destroy({ where: { id: req.params.id } });
+    const allShows = await Show.findAll();
+    res.json(allShows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+showRouter.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 module.exports = { showRouter };
--- routes/users.js
+++ routes/users.js
@@ -6,43 +6,68 @@ const userRouter = express.Router();
 userRouter.use(express.json());
 userRouter.use(express.urlencoded());
 
-userRouter.get("/", async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
-});
-
-userRouter.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
-  res.json(user);
-});
-
-userRouter.get("/:id/shows", async (req, res) => {
-  const usersShow = await User.findOne({
-    where: { id: req.params.id },
-    include: Show,
-  });
-  res.json(usersShow.shows);
-});
-
-userRouter.post("/", [check("username").isEmail()], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.json({ error: errors.array() });
-  } else {
-    const newUser = await User.create(req.body);
-    res.json(newUser);
+userRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    next(error);
   }
 });
 
-userRouter.put("/:id/shows/:showId", async (req, res) => {
-  const user = await User.findOne({
-    where: { id: req.params.id },
-    include: Show,
-  });
-  const show = await Show.findByPk(req.params.showId);
-  await user.addShow(show);
+userRouter.get("/:id", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  res.json(user.shows);
+userRouter.get("/:id/shows", async (req, res, next) => {
+  try {
+    const usersShow = await User.findOne({
+      where: { id: req.params.id },
+      include: Show,
+    });
+    res.json(usersShow.shows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post("/", [check("username").isEmail()], async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ error: errors.array() });
+    } else {
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.put("/:id/shows/:showId", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { id: req.params.id },
+      include: Show,
+    });
+    const show = await Show.findByPk(req.params.showId);
+    await user.addShow(show);
+
+    res.json(user.shows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 module.exports = { userRouter };
